60fe9d8959b86c0d459b5811dc5364885a14e61b
Indra <iwahyu@student.telkomuniversity.ac.id>
2024-12-15 23:06:22 +0700

    [Feat] Added endpoint to favorite a menu

--- src/routes/userCreationRoutes.js
+++ src/routes/userCreationRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const { getUserCreations, createUserCreation, scanNutrientFacts } = require('../controllers/userCreationController');
+const { getUserCreations, createUserCreation, scanNutrientFacts, favoriteUserCreation } = require('../controllers/userCreationController');
 const authMiddleware = require('../middlewares/authMiddleware');
 
 const router = express.Router();
 
 router.get('/', authMiddleware, getUserCreations);
 router.post('/', authMiddleware, createUserCreation);
+router.post('/favorite', authMiddleware, favoriteUserCreation);
 router.post('/nutrient-facts', authMiddleware, scanNutrientFacts);
 
 module.exports = router;
