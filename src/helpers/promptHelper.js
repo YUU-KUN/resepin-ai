efa5290881ada9f2e64b4f05bee69c6c225c88a3
Indra <iwahyu@student.telkomuniversity.ac.id>
2024-12-15 23:07:21 +0700

    [Refactor] Clean the unused codes

--- src/helpers/promptHelper.js
+++ src/helpers/promptHelper.js
@@ -48,39 +48,3 @@ exports.scanNutrientFactsPrompt = () => {
         if you  can't create any dishes, respond with empty array.
     `;
 }
-    
-    
-    // const conditionalPrompt = ingredients ? `with the following ingredients: ${ingredients.join(', ')}` : 'with the ingredients on the picture';
-
-    // I need each cooking menu to have the following fields: name, ingredients, instruction, and nutrientValue.
-    // with the type of each field as follows:
-
-    // Make sure the JSON data is valid and complete.
-    //     
-    // Please create a menu for me that includes at least one dish that uses all of these ingredients, and 2 more dishes that use some of these ingredients. so you can create max 3 dishes.
-    
-    // Please provide the name of the dish, the ingredients used, the instruction to make the dish, and the nutrient value of the dish.
-    // Please provide the response in JSON format.
-    // I need each cooking menu to have the following fields: name, ingredients, instruction, and nutrientValue.
-    //     with the type of each field as follows:
-    //     name: string,
-    //     ingredients: [
-    //         {
-    //             name: string,
-    //             quantity: number,
-    //             unit: string
-    //         }
-    //     ],
-    //     instruction: string,
-    //     nutrientValue: {
-    //         calories: number,
-    //         protein: number,
-    //         fat: number,
-    //         carbohydrates: number,
-    //         fiber: number,
-    //         sugar: number,
-    //         sodium: number
-    //         cholesterol: number,
-    //         etc...
-    //     }
-    //     Respond with only the JSON data, do not include any other text.
--- src/lib/vertex.js
+++ src/lib/vertex.js
@@ -1,18 +1,14 @@
 
 const {
-    FunctionDeclarationSchemaType,
     HarmBlockThreshold,
     HarmCategory,
     VertexAI
 } = require('@google-cloud/vertexai');
-const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
 const project = 'resepin';
 const location = 'us-central1';
 const textModel = 'gemini-1.5-pro';
 const visionModel = 'gemini-1.0-pro-vision';
-// const visionModel = 'imagen-3.0-fast-generate-001';
 const vertexAI = new VertexAI({ project: project, location: location });
-// const genAI = new GoogleGenerativeAI("YOUR_API_KEY");
 
 const generationConfig = { maxOutputTokens: 256 }
 const safetySettings = [{ category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT, threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE }]
